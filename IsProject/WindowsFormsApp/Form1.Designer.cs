e98f76ff7fe6bc3967ad28af6d59a2e0257b6a05
Mateo Narváez <m.narvaez2207@gmail.com>
2020-12-18 02:44:32 +0000

    XML to JSON
    Windows form creado con open dialog para encontrar el archivo xml, y un boton para transformarlo a json,
    
    Open dialogs creados para Excel

--- IsProject/WindowsFormsApp/Form1.Designer.cs
+++ IsProject/WindowsFormsApp/Form1.Designer.cs
@@ -29,13 +29,107 @@ namespace WindowsFormsApp
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.xmlDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtXml = new System.Windows.Forms.TextBox();
+            this.btnSelectXML = new System.Windows.Forms.Button();
+            this.btnXmlToJson = new System.Windows.Forms.Button();
+            this.excelDialog = new System.Windows.Forms.OpenFileDialog();
+            this.txtExcel = new System.Windows.Forms.TextBox();
+            this.btnSelectExcel = new System.Windows.Forms.Button();
+            this.btnExcelToJson = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // xmlDialog
+            // 
+            this.xmlDialog.FileName = "xml";
+            // 
+            // txtXml
+            // 
+            this.txtXml.Location = new System.Drawing.Point(23, 88);
+            this.txtXml.Name = "txtXml";
+            this.txtXml.Size = new System.Drawing.Size(180, 20);
+            this.txtXml.TabIndex = 0;
+            // 
+            // btnSelectXML
+            // 
+            this.btnSelectXML.Location = new System.Drawing.Point(237, 85);
+            this.btnSelectXML.Name = "btnSelectXML";
+            this.btnSelectXML.Size = new System.Drawing.Size(103, 23);
+            this.btnSelectXML.TabIndex = 1;
+            this.btnSelectXML.Text = "Select Xml";
+            this.btnSelectXML.UseVisualStyleBackColor = true;
+            this.btnSelectXML.Click += new System.EventHandler(this.btnSelectXML_Click);
+            // 
+            // btnXmlToJson
+            // 
+            this.btnXmlToJson.Location = new System.Drawing.Point(365, 85);
+            this.btnXmlToJson.Name = "btnXmlToJson";
+            this.btnXmlToJson.Size = new System.Drawing.Size(106, 23);
+            this.btnXmlToJson.TabIndex = 2;
+            this.btnXmlToJson.Text = "Convert to JSON";
+            this.btnXmlToJson.UseVisualStyleBackColor = true;
+            this.btnXmlToJson.Click += new System.EventHandler(this.btnXmlToJson_Click);
+            // 
+            // excelDialog
+            // 
+            this.excelDialog.FileName = "excelDialog";
+            // 
+            // txtExcel
+            // 
+            this.txtExcel.Location = new System.Drawing.Point(23, 150);
+            this.txtExcel.Name = "txtExcel";
+            this.txtExcel.Size = new System.Drawing.Size(180, 20);
+            this.txtExcel.TabIndex = 3;
+            this.txtExcel.TextChanged += new System.EventHandler(this.txtExcel_TextChanged);
+            // 
+            // btnSelectExcel
+            // 
+            this.btnSelectExcel.Location = new System.Drawing.Point(237, 146);
+            this.btnSelectExcel.Name = "btnSelectExcel";
+            this.btnSelectExcel.Size = new System.Drawing.Size(103, 23);
+            this.btnSelectExcel.TabIndex = 4;
+            this.btnSelectExcel.Text = "Select Excel";
+            this.btnSelectExcel.UseVisualStyleBackColor = true;
+            this.btnSelectExcel.Click += new System.EventHandler(this.btnSelectExcel_Click);
+            // 
+            // btnExcelToJson
+            // 
+            this.btnExcelToJson.Location = new System.Drawing.Point(365, 146);
+            this.btnExcelToJson.Name = "btnExcelToJson";
+            this.btnExcelToJson.Size = new System.Drawing.Size(106, 23);
+            this.btnExcelToJson.TabIndex = 5;
+            this.btnExcelToJson.Text = "Convert to JSON";
+            this.btnExcelToJson.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(483, 258);
+            this.Controls.Add(this.btnExcelToJson);
+            this.Controls.Add(this.btnSelectExcel);
+            this.Controls.Add(this.txtExcel);
+            this.Controls.Add(this.btnXmlToJson);
+            this.Controls.Add(this.btnSelectXML);
+            this.Controls.Add(this.txtXml);
+            this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.OpenFileDialog xmlDialog;
+        private System.Windows.Forms.TextBox txtXml;
+        private System.Windows.Forms.Button btnSelectXML;
+        private System.Windows.Forms.Button btnXmlToJson;
+        private System.Windows.Forms.OpenFileDialog excelDialog;
+        private System.Windows.Forms.TextBox txtExcel;
+        private System.Windows.Forms.Button btnSelectExcel;
+        private System.Windows.Forms.Button btnExcelToJson;
     }
 }
 
--- IsProject/WindowsFormsApp/Form1.cs
+++ IsProject/WindowsFormsApp/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Xml;
 
 namespace WindowsFormsApp
 {
@@ -16,5 +18,56 @@ namespace WindowsFormsApp
         {
             InitializeComponent();
         }
+
+        private void btnSelectXML_Click(object sender, EventArgs e)
+        {
+            this.xmlDialog.Title = "Selecionar XML";
+            xmlDialog.InitialDirectory = @"C:\Users\";
+            xmlDialog.Filter = "XML files (*.xml)|*.xml";
+            xmlDialog.CheckFileExists = true;
+            xmlDialog.CheckPathExists = true;
+
+            if (xmlDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtXml.Text = xmlDialog.FileName;
+            }
+        }
+        private void btnSelectExcel_Click(object sender, EventArgs e)
+        {
+            this.excelDialog.Title = "Selecionar Excel File";
+            excelDialog.InitialDirectory = @"C:\Users\";
+            excelDialog.Filter = "Excel Files (*.xlsx)|Excel Files(*.xls)|Excel Files(*.xlsm)|*.xls;*.xlsx;*.xlsm";
+            excelDialog.CheckFileExists = true;
+            excelDialog.CheckPathExists = true;
+
+            if (excelDialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+            {
+                txtExcel.Text = excelDialog.FileName;
+            }
+        }
+
+        private void btnXmlToJson_Click(object sender, EventArgs e)
+        {
+            XmlDocument doc = new XmlDocument();
+            String xmlFileName = txtXml.Text;
+            doc.Load(xmlFileName);
+
+            string json = JsonConvert.SerializeXmlNode(doc);
+
+            Console.WriteLine(json);
+
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void txtExcel_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
     }
 }
